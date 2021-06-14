47582b7dbf9f801bec5a5af76d9f7f9428dab64f
hollyhbrady <hollyhbrady@gmail.com>
2021-06-14 20:20:07 +0100

    Dodgems - Test 36, 37 and 38 passing

--- src/test/java/attractions/DodgemTest.java
+++ src/test/java/attractions/DodgemTest.java
@@ -2,18 +2,22 @@ package attractions;
 
 import org.junit.Before;
 import org.junit.Test;
+import people.Visitor;
 
 import static org.junit.Assert.assertEquals;
 
 public class DodgemTest {
 
     Dodgems dodgems;
+    Visitor young;
+    Visitor older;
 
     @Before
     public void setUp() throws Exception {
         dodgems = new Dodgems("Bumper Cars", 5);
+        young = new Visitor(10, 1.8, 100.00);
+        older = new Visitor(20, 2.1, 100.00);
     }
-
 
     @Test
     public void hasName() {
@@ -28,5 +32,20 @@ public class DodgemTest {
     @Test
     public void hasVisitCount() {
         assertEquals(0, dodgems.getVisitCount());
+    }
+
+    @Test
+    public void hasDefaultPrice(){
+        assertEquals(4.5, dodgems.defaultPrice(),0.0);
+    }
+
+    @Test
+    public void priceForYoung(){
+        assertEquals(2.25, dodgems.priceFor(young), 0.0);
+    }
+
+    @Test
+    public void priceForOlder(){
+        assertEquals(4.5, dodgems.priceFor(older), 0.0);
     }
 }
