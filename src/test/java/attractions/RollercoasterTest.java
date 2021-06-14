a2f5057ed932ccc91b672f15d8c598ae5ee94fae
hollyhbrady <hollyhbrady@gmail.com>
2021-06-14 18:17:22 +0100

    Rollercoaster - Test 29, 30, 31, 32 passing

--- src/test/java/attractions/RollercoasterTest.java
+++ src/test/java/attractions/RollercoasterTest.java
@@ -2,16 +2,25 @@ package attractions;
 
 import org.junit.Before;
 import org.junit.Test;
+import people.Visitor;
 
 import static org.junit.Assert.assertEquals;
 
 public class RollercoasterTest {
 
     RollerCoaster rollerCoaster;
+    Visitor child;
+    Visitor adult;
+    Visitor shortadult;
+    Visitor young;
 
     @Before
     public void setUp() {
         rollerCoaster = new RollerCoaster("Blue Ridge", 10);
+        child = new Visitor(10, 1.2, 40.0);
+        adult = new Visitor(20, 1.6, 100.00);
+        shortadult = new Visitor(25, 1.4, 100.00);
+        young = new Visitor(10, 1.8, 100.00);
     }
 
     @Test
@@ -27,5 +36,24 @@ public class RollercoasterTest {
     @Test
     public void hasVisitCount() {
         assertEquals(0, rollerCoaster.getVisitCount());
+    }
+
+    @Test
+    public void isAllowedToTrue(){
+        assertEquals(true, rollerCoaster.isAllowedTo(adult));
+    }
+    @Test
+    public void isAllowedToFalseAgeHeight(){
+        assertEquals(false, rollerCoaster.isAllowedTo(child));
+    }
+
+    @Test
+    public void isAllowedToFalseAge(){
+        assertEquals(false, rollerCoaster.isAllowedTo(young));
+    }
+
+    @Test
+    public void isAllowedToFalseHeight(){
+        assertEquals(false, rollerCoaster.isAllowedTo(shortadult));
     }
 }
