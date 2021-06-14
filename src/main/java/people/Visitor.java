68a467fff79e41127a43e9676cef8cc58036c1a3
hollyhbrady <hollyhbrady@gmail.com>
2021-06-14 21:13:32 +0100

    ThemePark - End point Extensions visitedAttractions ArrayList

--- src/main/java/people/Visitor.java
+++ src/main/java/people/Visitor.java
@@ -1,10 +1,16 @@
 package people;
 
+import attractions.Attraction;
+
+import java.util.ArrayList;
+
 public class Visitor {
 
     private int age;
     private double height;
     private double money;
+    private ArrayList<Attraction> visitedAttractions;
+
 
     public Visitor(int age, double height, double money) {
         this.age = age;
@@ -23,4 +29,13 @@ public class Visitor {
     public double getMoney() {
         return money;
     }
+
+    public void addAttraction(Visitor visitor, Attraction attraction){
+        this.visitedAttractions.add(attraction);
+    }
+
+    public int getVisitedAttractions(){
+        return this.visitedAttractions.size();
+    }
+
 }
--- src/test/java/people/VisitorTest.java
+++ src/test/java/people/VisitorTest.java
@@ -1,16 +1,22 @@
 package people;
 
+import attractions.Attraction;
+import attractions.Dodgems;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.ArrayList;
 
 import static org.junit.Assert.assertEquals;
 
 public class VisitorTest {
 
     Visitor visitor;
+    Attraction dodgems;
 
     @Before
     public void before(){
+        dodgems = new Dodgems("Bumper Cars", 5);
         visitor = new Visitor(14, 1.2, 40.0);
     }
 
@@ -27,5 +33,16 @@ public class VisitorTest {
     @Test
     public void hasMoney() {
         assertEquals(40.0, visitor.getMoney(), 0.1);
+    }
+
+    @Test
+    public void attractionListStartsEmpty(){
+        assertEquals(0, visitor.getVisitedAttractions());
+    }
+
+    @Test
+    public void canAddToVisitedAttractions(){
+        visitor.addAttraction(visitor, dodgems);
+        assertEquals(1, visitor.getVisitedAttractions());
     }
 }
