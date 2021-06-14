3bb3c46b61dc9e9c023e409b79818397a3fd5097
hollyhbrady <hollyhbrady@gmail.com>
2021-06-14 18:05:11 +0100

    TobaccoStall - Test 27 and 28 passing

--- src/test/java/stalls/TobaccoStallTest.java
+++ src/test/java/stalls/TobaccoStallTest.java
@@ -2,16 +2,21 @@ package stalls;
 
 import org.junit.Before;
 import org.junit.Test;
+import people.Visitor;
 
 import static org.junit.Assert.assertEquals;
 
 public class TobaccoStallTest {
 
     TobaccoStall tobaccoStall;
+    Visitor child;
+    Visitor adult;
 
     @Before
     public void setUp() throws Exception {
         tobaccoStall = new TobaccoStall("Jacks Drum", "Jack Jarvis", ParkingSpot.B1);
+        child = new Visitor(14, 1.2, 40.0);
+        adult = new Visitor(20, 1.6, 100.00);
     }
 
     @Test
@@ -27,5 +32,15 @@ public class TobaccoStallTest {
     @Test
     public void hasParkingSpot() {
         assertEquals(ParkingSpot.B1, tobaccoStall.getParkingSpot());
+    }
+
+    @Test
+    public void isAllowedToTrue(){
+        assertEquals(true, tobaccoStall.isAllowedTo(adult));
+    }
+
+    @Test
+    public void isAllowedToFalse(){
+        assertEquals(false, tobaccoStall.isAllowedTo(child));
     }
 }
