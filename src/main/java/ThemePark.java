403a9dc57616554f609a7ff3bd8d7d8bc3d5629c
hollyhbrady <hollyhbrady@gmail.com>
2021-06-14 20:57:08 +0100

    ThemePark - Test 39 and 40 passing

--- src/main/java/ThemePark.java
+++ src/main/java/ThemePark.java
@@ -1,2 +1,40 @@
+import attractions.Attraction;
+import behaviours.IReviewed;
+import stalls.Stall;
+
+import java.util.ArrayList;
+
 public class ThemePark {
+
+    private ArrayList<Attraction> attractions;
+    private ArrayList<Stall> stalls;
+
+    public ThemePark() {
+    this.attractions = new ArrayList<>();
+    this.stalls = new ArrayList<>();
+    }
+
+    public int getAttractions() {
+        return this.attractions.size();
+    }
+
+    public int getStalls() {
+        return this.stalls.size();
+    }
+
+    public void addAttraction(Attraction attraction){
+        this.attractions.add(attraction);
+    }
+
+    public void addStall(Stall stall){
+        this.stalls.add(stall);
+    }
+
+    public ArrayList<IReviewed> getAllReviewed(){
+        ArrayList<IReviewed> reviewed = new ArrayList<>();
+        reviewed.addAll(attractions);
+        reviewed.addAll(stalls);
+        return reviewed;
+    }
+
 }
---
+++ src/test/java/ThemeParkTest.java
@@ -0,0 +1,35 @@
+import attractions.Attraction;
+import attractions.Dodgems;
+import org.junit.Before;
+import org.junit.Test;
+import stalls.CandyflossStall;
+import stalls.ParkingSpot;
+import stalls.Stall;
+
+import static org.junit.Assert.assertEquals;
+
+public class ThemeParkTest {
+
+    ThemePark themePark;
+    Attraction dodgems;
+    Stall candyflossStall;
+
+    @Before
+    public void before(){
+        dodgems = new Dodgems("Bumper Cars", 5);
+        candyflossStall = new CandyflossStall("Candy Land", "Harry Belafonte", ParkingSpot.A1, 8);
+        themePark = new ThemePark();
+    }
+
+    @Test
+    public void canAddAttractions(){
+        themePark.addAttraction(dodgems);
+        assertEquals(1, themePark.getAttractions());
+    }
+
+    @Test
+    public void canAddStalls(){
+        themePark.addStall(candyflossStall);
+        assertEquals(1, themePark.getStalls());
+    }
+}
